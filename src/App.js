708f54056e359210b0e9751514afcfa0ff2e0c6d
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-04 17:27:12 +0530

    fix - build deploy fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import { useEffect } from "react";
 import Login from "./components/Login/Login";
-import { useTheme } from "./context/theme-context";
-import { getBgColor } from "./utils/theme-util";
 
 const App = () => {
-    const { theme } = useTheme()
-
-    useEffect(() => {
-        // document
-        // document.body.classList.add(getBgColor(theme))
-    }, [theme])
 
     return (
         <>
