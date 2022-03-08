97d9fa24c6495f743e2a7cfaa82a14c2fcd91d41
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-08 18:22:14 +0530

    refactor - reusable components used in product page

---
+++ src/components/Products/index.js
@@ -0,0 +1,13 @@
+import ProductArticle from "./ProductArticle";
+import ProductsFilter from "./ProductsFilter";
+import ProductsHeader from "./ProductsHeader";
+import ProductsSection from "./ProductsSection";
+import ProductsPg from "./ProductsPg";
+
+export {
+    ProductArticle,
+    ProductsFilter,
+    ProductsHeader,
+    ProductsPg,
+    ProductsSection
+}
