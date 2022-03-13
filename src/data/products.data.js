09257981471fcaca51c052c0c2d7e19947615ae7
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-13 17:20:59 +0530

    feat - size updated to array of sizes

--- src/data/products.data.js
+++ src/data/products.data.js
@@ -5,7 +5,7 @@ export const productsArr = [
         id: 1,
         name: 'zoom freak 3',
         company: 'nike',
-        size: 9,
+        size: [9, 10, 11],
         price: 11995,
         offerPrice: 10000,
         img: {
@@ -14,13 +14,14 @@ export const productsArr = [
             sizes: productsImgArr[0].sizes
         },
         rating: 4,
-        gender: 'male'
+        gender: 'male',
+        quantity: 3
     },
     {
         id: 2,
         name: 'air jordan 36',
         company: 'nike',
-        size: 10,
+        size: [9, 10, 11],
         price: 14995,
         img: {
             srcSet: productsImgArr[1].srcSet,
@@ -28,13 +29,14 @@ export const productsArr = [
             sizes: productsImgArr[0].sizes
         },
         rating: 5,
-        gender: 'male'
+        gender: 'male',
+        quantity: 30
     },
     {
         id: 3,
         name: 'air max 97 se',
         company: 'nike',
-        size: 9,
+        size: [9, 10, 11],
         price: 9996,
         img: {
             srcSet: productsImgArr[2].srcSet,
@@ -42,13 +44,14 @@ export const productsArr = [
             sizes: productsImgArr[0].sizes
         },
         rating: 3,
-        gender: 'male'
+        gender: 'male',
+        quantity: 18
     },
     {
         id: 4,
         name: 'fontaka waffle',
         company: 'nike',
-        size: 9,
+        size: [9, 10],
         price: 6778,
         offerPrice: 5000,
         img: {
@@ -57,13 +60,14 @@ export const productsArr = [
             sizes: productsImgArr[0].sizes
         },
         rating: 4,
-        gender: 'female'
+        gender: 'female',
+        quantity: 25
     },
     {
         id: 5,
         name: 'air max 90 se',
         company: 'nike',
-        size: 10,
+        size: [9, 10],
         price: 12345,
         img: {
             srcSet: productsImgArr[4].srcSet,
@@ -71,6 +75,7 @@ export const productsArr = [
             sizes: productsImgArr[0].sizes
         },
         rating: 5,
-        gender: 'female'
+        gender: 'female',
+        quantity: 20
     },
 ]
