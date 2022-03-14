02bbd179dbd9391ed217bd4b04bc230676549a0e
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-14 18:51:56 +0530

    fix - merge conflicts removed

---
+++ src/data/wishlist.img.js
@@ -0,0 +1,12 @@
+import wishlistImg1M from '../assets/1-m.jpg'
+import wishlistImg1S from '../assets/1-s.jpg'
+
+export const wishlistImgArr = [
+    {
+        id: 1,
+        alt: 'sneaker',
+        imgSet: `${wishlistImg1M} 200w, ${wishlistImg1S} 150w`,
+        sizeM: '200px',
+        sizeS: '150px'
+    },
+]
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { ProductsProvider } from './contexts/products.context';
 import { ThemeProvider } from './contexts/theme.context';
 import { CartProvider } from './contexts/cart.context';
 import './stylesheet/style.css'
+import './stylesheet/wishlist.css'
 
 ReactDOM.render(
   <ThemeProvider>
