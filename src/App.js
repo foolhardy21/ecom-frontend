f6ca6d10fc9dd5747f5734282dc03d0f907fa0ca
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-15 14:08:12 +0530

    fix - merge conflicts removed

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ import Signup from './components/Signup/Signup';
 import Signup from './components/Signup/Signup';
 =======
 import { Signup } from './components/Signup';
+<<<<<<< HEAD
 >>>>>>> d6d9193 (refactor - util files updated)
+||||||| parent of 226e177 (fix - merge conflicts removed)
+=======
+import { ProductsPg } from "./components/Products";
+>>>>>>> 226e177 (fix - merge conflicts removed)
 
 const App = () => {
 
   return (
+<<<<<<< HEAD
     <>
       <Login />
       <Signup />
     </>
+||||||| parent of 226e177 (fix - merge conflicts removed)
+
+    <Signup />
+
+=======
+    <>
+      <Signup />
+      <ProductsPg />
+    </>
+>>>>>>> 226e177 (fix - merge conflicts removed)
   );
 }
 
