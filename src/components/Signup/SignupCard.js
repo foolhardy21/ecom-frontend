65227168ad59a7701d98839b1239a4227af67cdf
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-15 14:01:52 +0530

    fix - merge conflicts removed

--- src/components/Signup/SignupCard.js
+++ src/components/Signup/SignupCard.js
@@ -3,23 +3,26 @@ import Card from "../Reusable/Card"
 import Text from "../Reusable/Text"
 import Button from "../Reusable/Button"
 import Icon from "../Reusable/Icon"
+import { getBgColor, getBorderColor, getTextColor } from "../../utils/theme"
+import { useTheme } from "../../contexts/theme.context"
 
 const SignupCard = () => {
+    const { theme } = useTheme()
 
     return (
 
         <Card classes='flx flx-column flx-center pd-lg mg-top-lg'>
 
-            <Text classes='txt-cap txt-lg'>
+            <Text classes={`txt-cap txt-lg ${getTextColor(theme)}`}>
                 signup
             </Text>
 
             <SignupForm />
 
-            <Button classes='btn-outlined txt-primary txt-md b-solid b-primary flx flx-center txt-cap pd-xs mg-top-xs'>
+            <Button classes={`btn-outlined ${getBgColor(theme)} ${getTextColor(theme)} ${getBorderColor(theme)} b-solid txt-md flx flx-center txt-cap pd-xs mg-top-xs`}>
                 already have an account
 
-                <Icon classes='icon-primary'>
+                <Icon classes={`${theme === 'light' ? 'icon-primary' : 'icon-secondary'}`}>
 
                     chevron_right
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheet/style.css'
 import App from './App';
-import './stylesheet/style.css'
 import { ThemeProvider } from './context/theme-context'
 
 ReactDOM.render(
