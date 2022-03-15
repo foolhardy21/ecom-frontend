3780b867bdd5f40593e8253da43ce8fc78b6eb41
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-15 14:07:49 +0530

    fix - merge conflicts removed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheet/style.css'
 import App from './App';
-import { ThemeProvider } from './context/theme-context'
+import { ThemeProvider } from './contexts/theme.context'
+import { ProductsProvider } from './contexts/products.context';
+import './stylesheet/style.css'
 
 ReactDOM.render(
   <ThemeProvider>
-    <App />
+    <ProductsProvider>
+      <App />
+    </ProductsProvider>
   </ThemeProvider>,
   document.getElementById('root')
 );
