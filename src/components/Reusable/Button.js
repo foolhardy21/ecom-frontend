fa008356f084222c430d6eeaf42ebac1f48d709a
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-15 13:30:38 +0530

    fix - merge conflicts removed

--- src/components/Reusable/Button.js
+++ src/components/Reusable/Button.js
@@ -1,7 +1,11 @@
+import { useTheme } from "../../contexts/theme.context"
+import { getTextColor } from "../../utils/theme.util"
+
 const Button = (props) => {
+    const { theme } = useTheme()
 
     return (
-        <button id={props.id} onClick={props.onClick} className={props.classes}>
+        <button id={props.id} onClick={props.onClick} className={`${getTextColor(theme)} ${props.classes}`}>
             {props.children}
         </button>
     )
